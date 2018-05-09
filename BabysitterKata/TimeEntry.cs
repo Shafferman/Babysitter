69b5aef27bc99c4345bc72dc03669beecfb85bac
Jeff Shafferman <jshafferman@pillartechnology.com>
2018-05-09 18:39:14 -0500

    * .gitignore: initial commit
    * Program.cs:
    * BabysitterKata.sln:
    * Tests.csproj:
    * packages.config:
    * TimeEntryTests.cs:
    * Program.cs:
    * TimeEntry.cs:
    * BabysitterKata.csproj:
    * AssemblyInfo.cs:

---
+++ BabysitterKata/TimeEntry.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace BabysitterKata
+{
+    public class TimeEntry
+    {
+        private DateTime started;
+        private DateTime ended;
+
+        public DateTime Start
+        {
+            get
+            {
+                return started;
+            }
+        }
+
+        public DateTime End
+        {
+            get
+            {
+                return ended;
+            }
+        }
+
+        public TimeEntry()
+        {
+        }
+
+        public void StartTime(DateTime dateTime)
+        {
+            started = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, 0, 0);
+        }
+
+        public void EndTime(DateTime dateTime)
+        {
+            ended = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour + 1, 0, 0);
+        }
+    }
+}
---
+++ Tests/TimeEntryTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BabysitterKata;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TimeEntryTests
+    {
+        private TimeEntry sut;
+
+        [SetUp]
+        public void Init()
+        {
+            sut = new TimeEntry();
+        }
+
+        [Test]
+        public void GivenStartTimeWhenTimeIsEnteredThenStartTimeShouldRoundDown()
+        {
+            sut.StartTime(new DateTime(2018, 1, 22, 12, 20, 15));
+
+            Assert.AreEqual(new DateTime(2018, 1, 22, 12, 0, 0), sut.Start);
+        }
+
+        [Test]
+        public void GivenEndedTimeWhenTimeIsEnteredThenEndTimeShouldRoundUp()
+        {
+            sut.EndTime(new DateTime(2018, 1, 22, 12, 20, 15));
+
+            Assert.AreEqual(new DateTime(2018, 1, 22, 13, 0, 0), sut.End);
+        }
+    }
+}
