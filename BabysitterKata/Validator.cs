97b0fd169d4d10612a141c713dec4d82ba100fe4
Jacob Feld <jfeld@pillartechnology.com>
2018-05-29 09:54:49 -0500

    Changes from thursday

---
+++ BabysitterKata/Validator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace BabysitterKata
+{
+    public class Validator
+    {
+        public Dictionary<string, DateTime> validate(Dictionary<string, string> input)
+        {
+            CultureInfo provider = CultureInfo.InvariantCulture;
+            
+            var validatedDictionary = new Dictionary<string, DateTime>();
+
+            try
+            {
+                validatedDictionary["start"] = DateTime.ParseExact(input["start"], "HHmm", provider);
+            }
+            catch (FormatException ex)
+            {
+                throw new FormatException("Start Time is invalid!");
+            }
+
+            validatedDictionary["end"] = DateTime.ParseExact(input["end"], "HHmm", provider);
+
+            return validatedDictionary;
+        }
+    }
+}
---
+++ Tests/ValidatorTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using BabysitterKata;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ValidatorTests
+    {
+
+        private Validator sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            sut = new Validator();
+        }
+
+        [Test]
+        public void ValidatorReturnsDateTimeDictionary()
+        {
+            var input = new Dictionary<string, string> {{"start", "1200"}, {"end", "1600"}};
+            var expectedStart = new DateTime(2018, 1, 1, 12, 0, 0);
+            var expectedEnd = new DateTime(2018, 1, 1, 16, 0, 0);
+            var expected = new Dictionary<string, DateTime> {{"start", expectedStart}, {"end", expectedEnd}};
+
+            var actual = sut.validate(input);
+            
+            Assert.AreEqual(expected["start"].Hour, actual["start"].Hour);
+            Assert.AreEqual(expected["end"].Hour, actual["end"].Hour);
+        }
+
+        [Test]
+        public void ValidatorThrowsErrorWhenStartTimeValueIsNotParsable()
+        {
+            var input = new Dictionary<string, string> {{"start", "I_AM_NOT_GOOD"}, {"end", "1600"}};
+            var expectedStart = new DateTime(2018, 1, 1, 12, 0, 0);
+            var expectedEnd = new DateTime(2018, 1, 1, 16, 0, 0);
+            var expected = new Dictionary<string, DateTime> {{"start", expectedStart}, {"end", expectedEnd}};
+            
+            var ex = Assert.Throws<FormatException>(() => sut.validate(input));
+            Assert.AreEqual("Start Time is invalid!", ex.Message);
+        }
+    }
+}
